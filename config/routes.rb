2d5ac4ecaa8b36b7ad6da34b6aae2e4029c27504
Usman Jamil <usman.jamil@live.co.uk>
2016-01-01 15:04:32 +0500

    removed duplicate :users

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,6 @@ Rails.application.routes.draw do
   # root :to => "forem/forums#index"
 
   root to: "home#index"
-
-  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
