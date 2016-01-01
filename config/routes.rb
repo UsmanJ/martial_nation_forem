4b912fdb267422319ad64079348a693a5cf14d9b
Usman Jamil <usman.jamil@live.co.uk>
2016-01-01 13:13:34 +0500

    added user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   # This line mounts Forem's routes at /forums by default.
   # This means, any requests to the /forums URL of your application will go to Forem::ForumsController#index.
   # If you would like to change where this extension is mounted, simply change the :at option to something different.
   #
   # We ask that you don't use the :as option here, as Forem relies on it being the default of "forem"
   mount Forem::Engine, :at => "/"
-  root :to => "forem/forums#index"
+  # root :to => "forem/forums#index"
+
+  root to: "home#index"
 
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
