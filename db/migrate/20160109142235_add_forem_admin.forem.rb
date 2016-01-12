d3652dc5a271371ebdab50018a005e9072e8db8d
Usman Jamil <usman.jamil@live.co.uk>
2016-01-12 16:37:14 +0000

    add_forem_admin.forem returned to its original state

--- db/migrate/20160109142235_add_forem_admin.forem.rb
+++ db/migrate/20160109142235_add_forem_admin.forem.rb
@@ -1,8 +1,8 @@
 # This migration comes from forem (originally 20120616193446)
 class AddForemAdmin < ActiveRecord::Migration
   def change
-    unless column_exists?(:user_class, :forem_admin)
-      add_column :user_class, :forem_admin, :boolean, :default => false
+    unless column_exists?(user_class, :forem_admin)
+      add_column user_class, :forem_admin, :boolean, :default => false
     end
   end
 
